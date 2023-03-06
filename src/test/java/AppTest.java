5ef73aad0a1078910ea4a751a74310c1e773cdee
kgpksh <tkdgns543@gmail.com>
2023-03-06 15:25:10 +0900

    AppTestRunner 클래스로 중복 제거

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -38,38 +38,15 @@ class AppTest {
     @Test
     @DisplayName("프로그램 시작 시 타이틀 출력 그리고 종료")
     public void t3() {
-        Scanner sc = TestUtil.genScanner("종료");
-
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-
-        new App(sc).run();
-
-        String string = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
-
-        assertThat(string)
-                .contains("== 명언 앱 ==")
-                .contains("명령) ")
-                .contains("프로그램이 종료되었습니다")
-                .doesNotContain("올바르지 않은 명령입니다");
+        assertThat(AppTestRunner.run(""))
+                .contains("== 명언 앱 ==");
 
     }
 
     @Test
     @DisplayName("잘못된 명령 입력에 대한 처리")
     public void t4() {
-        Scanner sc = TestUtil.genScanner("""
-                종료2
-                종료
-                """.stripIndent().trim());
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-
-        new App(sc).run();
-
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
-
-        assertThat(rs)
+        assertThat(AppTestRunner.run("종료2"))
                 .contains("== 명언 앱 ==")
                 .contains("명령) ")
                 .contains("올바르지 않은 명령입니다");
