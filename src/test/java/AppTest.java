194e5cd8876994140dcb61e6c624109deb13d18f
kgpksh <tkdgns543@gmail.com>
2023-03-06 15:02:47 +0900

    화면에 문자열 출력 하지 않고 테스트

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -15,5 +16,21 @@ class AppTest {
         String cmd = sc.nextLine().trim();
         assertThat(cmd).isEqualTo("안녕");
 
+    }
+
+    @Test
+    @DisplayName("출력을 모니터에 하지 않고 문자열로 얻기")
+    public void t2() {
+        // System.out에 대한 화면 출력 금지
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("안녕");
+
+        // 그 동안 출력되지 않던 내용들을 문자열로 반환
+        String rs = output.toString();
+
+        // System.out에 대한 화면 출력
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs).isEqualTo("안녕");
     }
 }
